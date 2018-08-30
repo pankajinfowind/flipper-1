dcbcc69b2086d60c4c4df51ece4e8c5e35f196ea
Muragijimana <beastar457@gmail.com>
2018-08-30 15:43:25 +0200

    w

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { AngularElectronPage } from './app.po';
 import { browser, element, by } from 'protractor';
 
-describe('angular-electron App', () => {
+describe('flipper App', () => {
   let page: AngularElectronPage;
 
   beforeEach(() => {

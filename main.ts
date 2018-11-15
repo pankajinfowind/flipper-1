01170b9819695f2540336c8979e3187bc94792ac
Muragijimana <beastar457@gmail.com>
2018-11-15 15:04:05 +0200

    puase hidding menus

--- main.ts
+++ main.ts
@@ -1,15 +1,14 @@
-import { app, BrowserWindow, Tray, Menu, ipcMain } from 'electron';
-import * as path from 'path';
-import * as url from 'url';
+import { app, BrowserWindow, Tray, Menu, ipcMain } from "electron";
+import * as path from "path";
+import * as url from "url";
 
 let win, serve;
 const args = process.argv.slice(1);
-serve = args.some(val => val === '--serve');
-if (process.mas) app.setName('Flipper');
+serve = args.some(val => val === "--serve");
+if (process.mas) app.setName("Flipper");
 const debug = /--debug/.test(process.argv[2]);
 makeSingleInstance();
 function createWindow() {
-
   //const electronScreen = screen;
   //const size = electronScreen.getPrimaryDisplay().workAreaSize;
   const windowOptions = {
@@ -17,43 +16,44 @@ function createWindow() {
     minWidth: 680,
     height: 840,
     title: app.getName(),
-    icon:null
-  }
+    icon: null
+  };
 
-  if (process.platform === 'linux') {
-    windowOptions.icon = path.join(__dirname, 'app-icon/png/512.png');
-  }else
-  if (process.platform === 'win32') {
-    windowOptions.icon = path.join(__dirname, 'app-icon/win/app.ico');
-  }else{
-    windowOptions.icon = path.join(__dirname, 'app-icon/mac/app.icns');
+  if (process.platform === "linux") {
+    windowOptions.icon = path.join(__dirname, "app-icon/png/512.png");
+  } else if (process.platform === "win32") {
+    windowOptions.icon = path.join(__dirname, "app-icon/win/app.ico");
+  } else {
+    windowOptions.icon = path.join(__dirname, "app-icon/mac/app.icns");
   }
   // Create the browser window.
   win = new BrowserWindow(windowOptions);
 
   if (serve) {
-    require('electron-reload')(__dirname, {
+    require("electron-reload")(__dirname, {
       electron: require(`${__dirname}/node_modules/electron`)
     });
-    win.loadURL('http://localhost:4200');
+    win.loadURL("http://localhost:4200");
   } else {
-    win.loadURL(url.format({
-      pathname: path.join(__dirname, 'dist/index.html'),
-      protocol: 'file:',
-      slashes: true
-    }));
+    win.loadURL(
+      url.format({
+        pathname: path.join(__dirname, "dist/index.html"),
+        protocol: "file:",
+        slashes: true
+      })
+    );
   }
 
- // Launch fullscreen with DevTools open, usage: npm run debug
- if (debug) {
-  win.webContents.openDevTools()
-  win.maximize()
-  require('devtron').install()
-}
-
-win.on('closed', () => {
-  win = null
-})
+  // Launch fullscreen with DevTools open, usage: npm run debug
+  if (debug) {
+    win.webContents.openDevTools();
+    win.maximize();
+    require("devtron").install();
+  }
+  win.setMenu(null);
+  win.on("closed", () => {
+    win = null;
+  });
 }
 
 // Make this app a single instance app.
@@ -63,70 +63,72 @@ win.on('closed', () => {
 //
 // Returns true if the current version of the app should quit instead of
 // launching.
-function makeSingleInstance () {
-  if (process.mas) return
+function makeSingleInstance() {
+  if (process.mas) return;
 
-  app.requestSingleInstanceLock()
+  app.requestSingleInstanceLock();
 
-  app.on('second-instance', () => {
+  app.on("second-instance", () => {
     if (win) {
-      if (win.isMinimized()) win.restore()
-      win.focus()
+      if (win.isMinimized()) win.restore();
+      win.focus();
     }
-
   });
 }
 
-let appIcon = null
+let appIcon = null;
 
-ipcMain.on('put-in-tray', (event) => {
-  const iconName = process.platform === 'win32' ? 'tray/windows-icon.png' : 'tray/iconTemplate.png'
-  const iconPath = path.join(__dirname, iconName)
-  appIcon = new Tray(iconPath)
+ipcMain.on("put-in-tray", event => {
+  const iconName =
+    process.platform === "win32"
+      ? "tray/windows-icon.png"
+      : "tray/iconTemplate.png";
+  const iconPath = path.join(__dirname, iconName);
+  appIcon = new Tray(iconPath);
 
-  const contextMenu = Menu.buildFromTemplate([{
-    label: 'Remove',
-    click: () => {
-      event.sender.send('tray-removed')
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: "Remove",
+      click: () => {
+        event.sender.send("tray-removed");
+      }
     }
-  }])
+  ]);
 
-  appIcon.setToolTip('Flipper in the tray.')
-  appIcon.setContextMenu(contextMenu)
-})
+  appIcon.setToolTip("Flipper in the tray.");
+  appIcon.setContextMenu(contextMenu);
+});
 
-ipcMain.on('remove-tray', () => {
-  appIcon.destroy()
-})
+ipcMain.on("remove-tray", () => {
+  appIcon.destroy();
+});
 
-app.on('window-all-closed', () => {
-  if (appIcon) appIcon.destroy()
-})
+app.on("window-all-closed", () => {
+  if (appIcon) appIcon.destroy();
+});
 
 try {
-
   // This method will be called when Electron has finished
   // initialization and is ready to create browser windows.
   // Some APIs can only be used after this event occurs.
-  app.on('ready', createWindow);
+  app.on("ready", createWindow);
 
   // Quit when all windows are closed.
-  app.on('window-all-closed', () => {
+  app.on("window-all-closed", () => {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
+    if (process.platform !== "darwin") {
       app.quit();
     }
   });
 
-  app.on('activate', () => {
+  app.on("activate", () => {
     // On OS X it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (win === null) {
       createWindow();
     }
   });
-
 } catch (e) {
   console.log(e);
   // Catch Error
@@ -135,72 +137,65 @@ try {
 
 /////////////////////////////////////////  MENU
 
-
-
 const menu = Menu.buildFromTemplate([
   {
-  label: app.getName(),
+    label: app.getName(),
     submenu: [
-      {role: 'about'},
-      {type: 'separator'},
-      {role: 'services', submenu: []},
-      {type: 'separator'},
-      {role: 'hide'},
-      {role: 'hideothers'},
-      {role: 'unhide'},
-      {type: 'separator'},
-      {role: 'quit'}
+      { role: "about" },
+      { type: "separator" },
+      { role: "services", submenu: [] },
+      { type: "separator" },
+      { role: "hide" },
+      { role: "hideothers" },
+      { role: "unhide" },
+      { type: "separator" },
+      { role: "quit" }
     ]
   },
   {
-    label: 'Edit',
+    label: "Edit",
     submenu: [
-      {role: 'undo'},
-      {role: 'redo'},
-      {type: 'separator'},
-      {role: 'cut'},
-      {role: 'copy'},
-      {role: 'paste'},
-      {role: 'pasteandmatchstyle'},
-      {role: 'delete'},
-      {role: 'selectall'}
+      { role: "undo" },
+      { role: "redo" },
+      { type: "separator" },
+      { role: "cut" },
+      { role: "copy" },
+      { role: "paste" },
+      { role: "pasteandmatchstyle" },
+      { role: "delete" },
+      { role: "selectall" }
     ]
   },
   {
-    label: 'View',
+    label: "View",
     submenu: [
-      {role: 'reload'},
-      {role: 'forcereload'},
-      {role: 'toggledevtools'},
-      {type: 'separator'},
-      {role: 'resetzoom'},
-      {role: 'zoomin'},
-      {role: 'zoomout'},
-      {type: 'separator'},
-      {role: 'togglefullscreen'}
+      { role: "reload" },
+      { role: "forcereload" },
+      { role: "toggledevtools" },
+      { type: "separator" },
+      { role: "resetzoom" },
+      { role: "zoomin" },
+      { role: "zoomout" },
+      { type: "separator" },
+      { role: "togglefullscreen" }
     ]
   },
   {
-    label: 'History',
-    submenu: [
-      {role: 'back'},
-      {role: 'forward'}
-    ]
+    label: "History",
+    submenu: [{ role: "back" }, { role: "forward" }]
   },
   {
-    role: 'window',
-    submenu: [
-      {role: 'minimize'},
-      {role: 'maximize'},
-      {role: 'close'}
-    ]
+    role: "window",
+    submenu: [{ role: "minimize" }, { role: "maximize" }, { role: "close" }]
   },
   {
-    role: 'help',
+    role: "help",
     submenu: [
       {
-        label: 'Learn More',
-        click () { require('electron').shell.openExternal('https://electronjs.org') }
+        label: "Learn More",
+        click() {
+          require("electron").shell.openExternal("https://electronjs.org");
+        }
       }
     ]
   }

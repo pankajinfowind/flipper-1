4c70c76b4cfcb638d2becef891e4afc7fa547516
Muragijimana <beastar457@gmail.com>
2018-12-01 12:45:05 +0200

    feat(basic feature): only basic functionality

--- main.ts
+++ main.ts
@@ -8,51 +8,25 @@ serve = args.some(val => val === "--serve");
 if (process.mas) app.setName("Flipper");
 const debug = /--debug/.test(process.argv[2]);
 makeSingleInstance();
-
 function createWindow() {
+  //const electronScreen = screen;
+  //const size = electronScreen.getPrimaryDisplay().workAreaSize;
   const windowOptions = {
     width: 1080,
+    minWidth: 680,
     height: 840,
     title: app.getName(),
-    icon: null,
-    show: false
+    icon: null
   };
 
-  if (serve) {
-    if (process.platform === "linux") {
-      windowOptions.icon = path.join(
-        __dirname,
-        "src/assets/app-icon/png/512.png"
-      );
-    } else if (process.platform === "win32") {
-      windowOptions.icon = path.join(
-        __dirname,
-        "src/assets/app-icon/win/app.ico"
-      );
-    } else {
-      windowOptions.icon = path.join(
-        __dirname,
-        "src/assets/app-icon/mac/app.icns"
-      );
-    }
+  if (process.platform === "linux") {
+    windowOptions.icon = path.join(__dirname, "app-icon/png/512.png");
+  } else if (process.platform === "win32") {
+    windowOptions.icon = path.join(__dirname, "app-icon/win/app.ico");
   } else {
-    if (process.platform === "linux") {
-      windowOptions.icon = path.join(
-        __dirname,
-        "dist/assets/app-icon/png/512.png"
-      );
-    } else if (process.platform === "win32") {
-      windowOptions.icon = path.join(
-        __dirname,
-        "dist/assets/app-icon/win/app.ico"
-      );
-    } else {
-      windowOptions.icon = path.join(
-        __dirname,
-        "dist/assets/app-icon/mac/app.icns"
-      );
-    }
+    windowOptions.icon = path.join(__dirname, "app-icon/mac/app.icns");
   }
+  // Create the browser window.
   win = new BrowserWindow(windowOptions);
 
   if (serve) {
@@ -70,8 +44,11 @@ function createWindow() {
     );
   }
 
+  // Launch fullscreen with DevTools open, usage: npm run debug
+  // win.webContents.openDevTools();
   if (debug) {
     win.webContents.openDevTools();
+
     win.maximize();
     require("devtron").install();
   }
@@ -80,6 +57,7 @@ function createWindow() {
     win = null;
   });
 }
+
 function makeSingleInstance() {
   if (process.mas) return;
 
@@ -96,18 +74,10 @@ function makeSingleInstance() {
 let appIcon = null;
 
 ipcMain.on("put-in-tray", event => {
-  let iconName = null;
-  if (serve) {
-    iconName =
-      process.platform === "win32"
-        ? "src/assets/tray-icon/windows-icon.png"
-        : "src/assets/tray-icon/iconTemplate.png";
-  } else {
-    iconName =
-      process.platform === "win32"
-        ? "dist/assets/tray-icon/windows-icon.png"
-        : "dist/assets/tray-icon/iconTemplate.png";
-  }
+  const iconName =
+    process.platform === "win32"
+      ? "tray/windows-icon.png"
+      : "tray/iconTemplate.png";
   const iconPath = path.join(__dirname, iconName);
   appIcon = new Tray(iconPath);
 
@@ -133,9 +103,11 @@ app.on("window-all-closed", () => {
 });
 
 try {
-  app.on("ready", () => {
-    createWindow();
-  });
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // Some APIs can only be used after this event occurs.
+  app.on("ready", createWindow);
+
   // Quit when all windows are closed.
   app.on("window-all-closed", () => {
     // On OS X it is common for applications and their menu bar
@@ -146,11 +118,17 @@ try {
   });
 
   app.on("activate", () => {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
     if (win === null) {
       createWindow();
     }
   });
-} catch (e) {}
+} catch (e) {
+  console.log(e);
+  // Catch Error
+  // throw e;
+}
 
 /////////////////////////////////////////  MENU
 
@@ -158,14 +136,13 @@ const menu = Menu.buildFromTemplate([
   {
     label: app.getName(),
     submenu: [
-      {
-        role: "about",
-        click() {
-          require("electron").shell.openExternal(
-            "https://flipper.yegobox.rw/abouts"
-          );
-        }
-      },
+      { role: "about" },
+      { type: "separator" },
+      { role: "services", submenu: [] },
+      { type: "separator" },
+      { role: "hide" },
+      { role: "hideothers" },
+      { role: "unhide" },
       { type: "separator" },
       { role: "quit" }
     ]
@@ -189,6 +166,7 @@ const menu = Menu.buildFromTemplate([
     submenu: [
       { role: "reload" },
       { role: "forcereload" },
+      { role: "toggledevtools" },
       { type: "separator" },
       { role: "resetzoom" },
       { role: "zoomin" },
@@ -196,10 +174,6 @@ const menu = Menu.buildFromTemplate([
       { type: "separator" },
       { role: "togglefullscreen" }
     ]
-  },
-  {
-    label: "Developer",
-    submenu: [{ role: "toggledevtools" }]
   },
   {
     label: "History",
@@ -215,9 +189,7 @@ const menu = Menu.buildFromTemplate([
       {
         label: "Learn More",
         click() {
-          require("electron").shell.openExternal(
-            "https://flipper.yegobox.rw/help"
-          );
+          require("electron").shell.openExternal("https://flipper.yegobox.rw");
         }
       }
     ]

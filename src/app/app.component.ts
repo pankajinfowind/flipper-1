aee65d00056ad378ed7d74bffcf5bd6a2bdd09b2
Muragijimana <beastar457@gmail.com>
2018-12-05 12:46:20 +0200

    fix unmerged conflict

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,18 +10,5 @@ import { TranslateService } from "@ngx-translate/core";
 export class AppComponent {
   constructor(private translate: TranslateService) {
     translate.setDefaultLang("en");
-<<<<<<< HEAD
-=======
-    console.log("AppConfig", AppConfig);
-
-    if (electronService.isElectron()) {
-      console.log("Mode electron");
-      console.log("Electron ipcRenderer", electronService.ipcRenderer);
-      console.log("NodeJS childProcess", electronService.childProcess);
-      console.log("version", electronService.app);
-    } else {
-      console.log("Mode web");
-    }
->>>>>>> c04d6071275138cad32978a2dcd3dc42e7270ece
   }
 }

1da4555b46bd3266e7228e58e9dc0c66c62e149b
Richard Mr <richard@yegobox.com>
2019-07-20 12:44:27 +0200

    re-learn

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:enexus/util/data.dart';
 import 'package:flutter/material.dart';
 import 'const/themeColor.dart';
 import 'lib/bloc/cartlistBloc.dart';
@@ -109,7 +110,7 @@ class FirstHalf extends StatelessWidget {
           title(),
           SizedBox(height: 30),
           searchBar(),
-          SizedBox(height: 45),
+          SizedBox(height: 28),
           categories(),
         ],
       ),
@@ -124,31 +125,31 @@ Widget categories() {
       scrollDirection: Axis.horizontal,
       children: <Widget>[
         CategoryListItem(
-          categoryIcon: Icons.bug_report,
+          categoryIcon: "assets/cm1.jpeg",
           categoryName: "Burgers",
-          availability: 12,
-          selected: true,
-        ),
-        CategoryListItem(
-          categoryIcon: Icons.bug_report,
-          categoryName: "Pizza",
           availability: 12,
           selected: false,
         ),
         CategoryListItem(
-          categoryIcon: Icons.bug_report,
+          categoryIcon: "assets/cm1.jpeg",
+          categoryName: "Pizza",
+          availability: 12,
+          selected: true,
+        ),
+        CategoryListItem(
+          categoryIcon: "assets/cm1.jpeg",
           categoryName: "Rolls",
           availability: 12,
           selected: false,
         ),
         CategoryListItem(
-          categoryIcon: Icons.bug_report,
+          categoryIcon: "assets/cm1.jpeg",
           categoryName: "Burgers",
           availability: 12,
           selected: false,
         ),
         CategoryListItem(
-          categoryIcon: Icons.bug_report,
+          categoryIcon: "assets/cm1.jpeg",
           categoryName: "Burgers",
           availability: 12,
           selected: false,
@@ -267,7 +268,7 @@ class CategoryListItem extends StatelessWidget {
     @required this.selected,
   }) : super(key: key);
 
-  final IconData categoryIcon;
+  final String categoryIcon;
   final String categoryName;
   final int availability;
   final bool selected;
@@ -277,21 +278,6 @@ class CategoryListItem extends StatelessWidget {
     return Container(
       margin: EdgeInsets.only(right: 20),
       padding: EdgeInsets.fromLTRB(10, 10, 10, 20),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(50),
-        color: selected ? Themes.color : Colors.white,
-        border: Border.all(
-            color: selected ? Colors.transparent : Colors.grey[200],
-            width: 1.5),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey[100],
-            blurRadius: 15,
-            offset: Offset(15, 0),
-            spreadRadius: 5,
-          )
-        ],
-      ),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: <Widget>[
@@ -303,31 +289,14 @@ class CategoryListItem extends StatelessWidget {
                 border: Border.all(
                     color: selected ? Colors.transparent : Colors.grey[200],
                     width: 1.5)),
-            child: Icon(
-              categoryIcon,
-              color: Colors.black,
-              size: 30,
+            child: CircleAvatar(
+              backgroundImage: AssetImage(
+                "$categoryIcon"
+              ),
+              radius: 25,
             ),
           ),
-          SizedBox(height: 10),
-          Text(
-            categoryName,
-            style: TextStyle(
-                fontWeight: FontWeight.w700, color: Colors.black, fontSize: 15),
-          ),
-          Container(
-            margin: EdgeInsets.fromLTRB(0, 6, 0, 10),
-            width: 1.5,
-            height: 15,
-            color: Colors.black26,
-          ),
-          Text(
-            availability.toString(),
-            style: TextStyle(
-              fontWeight: FontWeight.w600,
-              color: Colors.black,
-            ),
-          )
+          SizedBox(height: 10)
         ],
       ),
     );
@@ -368,19 +337,12 @@ Widget title() {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Text(
-            "Food",
+            "Flipper",
             style: TextStyle(
               fontWeight: FontWeight.w700,
               fontSize: 30,
             ),
-          ),
-          Text(
-            "Delivery",
-            style: TextStyle(
-              fontWeight: FontWeight.w200,
-              fontSize: 30,
-            ),
-          ),
+          )
         ],
       )
     ],

2cb86d3aa75ecd5ff76166df7ddb92503f47a46f
Richard Mr <richard@yegobox.com>
2019-07-21 11:54:59 +0200

    working on login

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,42 @@ class _Login extends StatelessWidget {
             SizedBox(height: 20.0),
             Column(
               children: <Widget>[
+                Image.asset('assets/cm1.jpeg'),
+                SizedBox(height: 20.0),
+                Text("Login")
+              ],
+            ),
+            SizedBox(height: 120.0),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'username',
+                filled: true,
+                border: OutlineInputBorder()
+              ),
+            ),
+            SizedBox(height: 12.0),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'password',
+                filled: true,
+                border: OutlineInputBorder()
+              ),
+              obscureText: true,
+            ),
+            ButtonBar(
+              children: <Widget>[
+                FlatButton(
+                  child: Text("Cancel"),
+                  onPressed: (){
 
+                  },
+                 ),
+                RaisedButton(
+                  color: Colors.blueAccent,
+                  textColor: Colors.white,
+                  child: Text("Next"),
+                  onPressed: (){},
+                )
               ],
             )
           ],

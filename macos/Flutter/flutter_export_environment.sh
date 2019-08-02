c55ca0bcb77d99afd4e4ad60fc4f94dd414f4c12
Richard Mr <richard@yegobox.com>
2019-08-02 19:14:49 +0200

    can run on both platform

--- macos/Flutter/flutter_export_environment.sh
+++ macos/Flutter/flutter_export_environment.sh
@@ -2,7 +2,9 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/richard/Documents/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/richard/StudioProjects/enexus"
+export "FLUTTER_TARGET=/Users/richard/StudioProjects/enexus/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_FRAMEWORK_DIR=/Users/richard/Documents/flutter/bin/cache/artifacts/engine/darwin-x64"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"

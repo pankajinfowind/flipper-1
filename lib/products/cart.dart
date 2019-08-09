19499706c67f98ae6253fd7c8788962a5c3b49b6
Richard Mr <richard@yegobox.com>
2019-08-09 16:28:07 +0200

    working on ui of cart

--- lib/products/cart.dart
+++ lib/products/cart.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Cart extends StatelessWidget {
+class Cart extends StatefulWidget {
+  @override
+  _CartState createState() => _CartState();
+}
+
+class _CartState extends State<Cart> {
+  static final TextEditingController _textController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _textController.text = "1";
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       appBar: AppBar(
         title: Text("POS"),
@@ -32,15 +44,16 @@ class Cart extends StatelessWidget {
                   SizedBox(width: 30),
                   SizedBox(
                     width: 100,
-                    child: FlatButton(
-                      color: Colors.blue[500],
-                      child: Center(
-                        child: Text(
-                          "10",
-                          textAlign: TextAlign.center,
-                        ),
+                    height: 40,
+                    child: TextField(
+                      autofocus: true,
+                      controller: _textController,
+                      decoration: InputDecoration(
+                        fillColor: Colors.white,
+                        labelText: 'Qty',
+                        filled: true,
+                        border: OutlineInputBorder(),
                       ),
-                      onPressed: () {},
                     ),
                   ),
                   SizedBox(width: 40),

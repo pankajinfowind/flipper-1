77482f4dd006dbae79ea27e3fa8466750489be44
StreamUpBox LtdRichard <richard@yegobox.com>
2019-11-08 14:15:42 +0200

    re-work in fine tuning code

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # enexus
 
-A new Flutter application.
+A new Flutter project.
 
 ## Getting Started
 
-This project is a an android,ios client for flipper application for inventory system
+This project is a starting point for a Flutter application.
 
 A few resources to get you started if this is your first Flutter project:
 
--
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
 
-For help getting started with Flipper, view our documentation
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,25 +1,13 @@
 import UIKit
 import Flutter
-//import RealmSwift
+
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-//    //# Iam going to write platform specific code that talk to the host about database changes.
-//    let realm = try! Realm()
-//    print(Realm.Configuration.defaultConfiguration.fileURL)
-//    
-//    let  product = Categories()
-//    product.name = "Richie"
-//    product.color = "gray"
-//    
-//    try! realm.write {
-//        realm.add(product)
-//    }
-    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }

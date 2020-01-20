f05c3b2a064fe5c376c8cb208a2db3a2fd3c781e
Richard Mura <richard@yegobox.com>
2020-01-20 17:12:31 +0200

    draft add product item screen

--- lib/home/add_item_screen.dart
+++ lib/home/add_item_screen.dart
@@ -1,6 +1,7 @@
-
 import 'package:flipper/presentation/common/common_app_bar.dart';
 import 'package:flipper/routes/router.gr.dart';
+import 'package:flipper/util/HexColor.dart';
+import 'package:flipper/util/validators.dart';
 import 'package:flutter/material.dart';
 
 class AddItemScreen extends StatefulWidget {
@@ -17,9 +18,7 @@ class _AddItemScreenState extends State<AddItemScreen> {
       appBar: CommonAppBar(
         title: "Create Item",
         actionButton: FlatButton(
-          onPressed: () {
-
-          },
+          onPressed: () {},
           child: Text(
             "Save",
             textAlign: TextAlign.center,
@@ -29,32 +28,188 @@ class _AddItemScreenState extends State<AddItemScreen> {
         ),
         icon: Icons.close,
         multi: 3,
-        bottomSpacer: 52,
+        bottomSpacer: 48,
       ),
-      body: Column(
+      body: ListView(
         children: <Widget>[
-            Align(
-              child: Column(
-                children: <Widget>[
-                  SizedBox(height: 10,),
-                  GestureDetector(
-                    onTap: (){
-                      Router.navigator.pushNamed(Router.editItemTitle);
-                    },
-                    child: Column(
+          Column(
+            children: <Widget>[
+              SizedBox(
+                height: 10,
+              ),
+              GestureDetector(
+                onTap: () {
+                  Router.navigator.pushNamed(Router.editItemTitle);
+                },
+                child: Container(
+                  height: 80,
+                  width: 80,
+                  color: Colors.yellow,
+                ),
+              ),
+              Text("New Item"),
+              Center(
+                child: Container(
+                  width: 300,
+                  child: TextFormField(
+                    style: TextStyle(color: Colors.black),
+                    validator: Validators.isStringHasMoreChars,
+                    onSaved: (name) {},
+                    decoration: InputDecoration(
+                        hintText: "Name", focusColor: Colors.blue),
+                  ),
+                ),
+              ),
+              Center(
+                child: Container(
+                  width: 300,
+                  child: ListTile(
+                    contentPadding: EdgeInsets.symmetric(horizontal: 0.3),
+                    leading: Text("Category"),
+                    trailing: Wrap(
                       children: <Widget>[
-                        Container(
-                          height: 80,
-                          width: 80,
-                          color: Colors.yellow,
-                        ),
-                        Text("New Item")
+                        Text("Select Category"),
+                        Icon(Icons.arrow_forward_ios)
                       ],
                     ),
                   ),
-                ],
+                ),
               ),
-            )
+              Center(
+                child: Container(
+                  width: 300,
+                  child: Divider(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Center(
+                child: Container(
+                  width: 300,
+                  child: Text("PRICE AND INVENTORY"),
+                ),
+              ),
+              Center(
+                child: Container(
+                  width: 300,
+                  child: Divider(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Center(
+                child: Container(
+                  width: 300,
+                  child: ListTile(
+                    contentPadding: EdgeInsets.symmetric(horizontal: 0.3),
+                    leading: Text("Unit Type"),
+                    trailing: Wrap(
+                      children: <Widget>[
+                        Text("Per Item"),
+                        Icon(Icons.arrow_forward_ios)
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Center(
+                child: Container(
+                  width: 300,
+                  child: Divider(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Center(
+                child: Container(
+                  width: 300,
+                  child: TextFormField(
+                    style: TextStyle(color: Colors.black),
+                    validator: Validators.isStringHasMoreChars,
+                    onSaved: (name) {},
+                    decoration: InputDecoration(
+                        hintText: "Price", focusColor: Colors.blue),
+                  ),
+                ),
+              ),
+              Center(
+                child: Container(
+                  width: 300,
+                  child: TextFormField(
+                    style: TextStyle(color: Colors.black),
+                    validator: Validators.isStringHasMoreChars,
+                    onSaved: (name) {},
+                    decoration: InputDecoration(
+                        hintText: "SKU", focusColor: Colors.blue),
+                  ),
+                ),
+              ),
+              Center(
+                child: Container(
+                  width: 300,
+                  child: ListTile(
+                    contentPadding: EdgeInsets.symmetric(horizontal: 0.3),
+                    leading: Text("Stock"),
+                    trailing: FlatButton(
+                      child: Text("Receive Stock"),
+                      onPressed: () {},
+                    ),
+                  ),
+                ),
+              ),
+              Center(
+                child: SizedBox(
+                  height: 50,
+                  width: 280,
+                  child: FlatButton(
+                    color: HexColor("#ecf0f1"),
+                    child: Text("Add Variation"),
+                    onPressed: () {
+                      Router.navigator.pushNamed(Router.takePictureScreen);
+                    },
+                  ),
+                ),
+              ),
+              Container(
+                height: 4,
+              ),
+              Center(
+                child: Container(
+                  width: 300,
+                  child: Divider(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                height: 4,
+              ),
+              Text("Leave the price blank to enter at the time of sale."),
+              Center(
+                child: Container(
+                  width: 300,
+                  child: Divider(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Center(
+                child: Container(
+                  width: 300,
+                  child: TextFormField(
+                    style: TextStyle(color: Colors.black),
+                    validator: Validators.isStringHasMoreChars,
+                    onSaved: (name) {},
+                    decoration: InputDecoration(
+                        hintText: "Description", focusColor: Colors.blue),
+                  ),
+                ),
+              ),
+              Container(
+                height: 64,
+              ),
+            ],
+          ),
         ],
       ),
     );

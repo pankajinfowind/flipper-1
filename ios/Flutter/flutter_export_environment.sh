96436a77ca6e70a03dbc4f023b56fa0a5fe6d855
Richard Muragijimana <beastar457@gmail.com>
2020-05-09 16:31:30 +0200

    clean env

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Android\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\RUGAMBA\Documents\flipper"
+export "FLUTTER_ROOT=C:\Users\Mica\Documents\dev-softwares\flutter_windows_v1.12.13+hotfix.9-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Mica\Documents\yegobox\flipper.mobile"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\Android\flutter\bin\cache\artifacts\engine\ios"
-export "FLUTTER_BUILD_NAME=1.4.29"
-export "FLUTTER_BUILD_NUMBER=1.4.29"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=C:\Users\Mica\Documents\dev-softwares\flutter_windows_v1.12.13+hotfix.9-stable\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.5.31"
+export "FLUTTER_BUILD_NUMBER=1.5.31"

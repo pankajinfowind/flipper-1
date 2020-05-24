e7633dbf17bd539a9d061659f86693ce6ef470a8
Richard Muragijimana <beastar457@gmail.com>
2020-05-24 12:51:46 +0200

    remove report as package

--- packages/reports/CHANGELOG.md
+++
@@ -1,3 +0,0 @@
-## 0.0.1
-
-* TODO(richard): Describe initial release.
--- packages/reports/example/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:reports/report_widget.dart';
-
-void main() {
-  runApp(ReportWidget());
-}

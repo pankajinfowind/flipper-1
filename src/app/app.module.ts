7d8f24a328625a712b3c22a0da87e31b26e746d8
Ganza respice <umujyi2019@gmail.com>
2020-06-30 11:49:36 +0200

    fixing menu active and update transaction date after transaction completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -56,6 +56,7 @@ export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
 export class AppModule {
   businessName: string;
 
+  
   constructor(private database: PouchDBService,
     private eventBus: FlipperEventBusService,
     private firestore: AngularFirestore) {

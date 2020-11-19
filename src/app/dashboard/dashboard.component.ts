8e0cd2c2536248020234bf7729a9a387fdd13146
Ganza respice <respinho2014@gmail.com>
2020-11-19 21:11:08 +0200

    buy fixed now you can create a business

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
-import { DashBoardEntries, fadeInAnimation, MainModelService, Business, Tables, Stock,
+import { Component, OnInit } from '@angular/core';
+import {
+  DashBoardEntries, fadeInAnimation, Business, Tables, Stock,
   Branch, CalculateTotalClassPipe, RoundNumberPipe, Order, OrderDetails,
-   Variant, Product, Taxes, PouchDBService } from '@enexus/flipper-components';
+  Variant, Product, Taxes, PouchDBService
+} from '@enexus/flipper-components';
 import { trigger, transition, useAnimation } from '@angular/animations';
 import { ModelService } from '@enexus/flipper-offline-database';
 import { CurrentUser } from '../core/guards/current-user';
@@ -16,38 +18,37 @@ import { CurrentUser } from '../core/guards/current-user';
     ]),
   ],
 })
-export class DashboardComponent implements OnInit{
+export class DashboardComponent implements OnInit {
 
 
   dashboardEntries: DashBoardEntries;
 
 
-  public branch: Branch | null;
   public totalStore = 0.00;
   public netProfit = 0.00;
   public grossProfits = 0.00;
   public totalRevenue = 0.00;
-
+  private branch: Branch;
   public topSoldItem = [];
-  public lowStockItem=[];
+  public lowStockItem = [];
   public defaultBusiness$: Business = null;
-  public defaultBranch:Branch=null;
+  public defaultBranch: Branch = null;
   // public currency = this.model.active<Business>(Tables.business) ? this.model.active<Business>(Tables.business).currency : 'RWF';
   constructor(private totalPipe: CalculateTotalClassPipe,
     private currentUser: CurrentUser,
-              private radomNumberPipe: RoundNumberPipe,
-              private query: ModelService,
-              private database: PouchDBService,
-               private model: MainModelService) {
+    private radomNumberPipe: RoundNumberPipe,
+    private query: ModelService,
+    private database: PouchDBService) {
 
-                console.log(this.currentUser.currentBusiness);
-              this.branch = this.model.active<Branch>(Tables.branch);
-              this.totalStore =0.00; //this.getStockValue();
-              this.netProfit = 0.00;//this.getNetProfit();
-              this.totalRevenue =0.00;// this.getTotalRevenues();
-              this.grossProfits = 0.00;//this.getGrossProfit();
-              this.topSoldItem =[];// this.topSoldItems();
-              this.lowStockItem =[];// this.getLowStocks();
+    console.log(this.currentUser.currentBusiness);
+    // FIXME: 
+    this.branch = null;
+    this.totalStore = 0.00; //this.getStockValue();
+    this.netProfit = 0.00;//this.getNetProfit();
+    this.totalRevenue = 0.00;// this.getTotalRevenues();
+    this.grossProfits = 0.00;//this.getGrossProfit();
+    this.topSoldItem = [];// this.topSoldItems();
+    this.lowStockItem = [];// this.getLowStocks();
 
   }
 
@@ -57,41 +58,41 @@ export class DashboardComponent implements OnInit{
   }
 
   async ngOnInit() {
-  
+
     await this.init();
   }
 
 
   public currentBusiness() {
     return this.database.currentBusiness().then(business => {
- 
+
       this.defaultBusiness$ = business;
     });
   }
   currentBranches() {
     return this.database.listBusinessBranches().then(branches => {
-      this.defaultBranch = branches.length > 0?branches[0]:0;
+      this.defaultBranch = branches.length > 0 ? branches[0] : 0;
     });
   }
 
   topSoldItems() {
-    const topSolds=[];
-    if(this.topSoldsItem().length > 0) {
-      this.topSoldsItem().forEach((item: OrderDetails,i)=> {
+    const topSolds = [];
+    // if(this.topSoldsItem().length > 0) {
+    //   this.topSoldsItem().forEach((item: OrderDetails,i)=> {
 
-        if(item.quantity) {
-          const x= {
-            id: i+1,
-            name: item.variantName,
-            updatedAt: 'Updated 5m ago',
-            items: item.quantity,
-            total: item.subTotal
-          };
-          topSolds.push(x);
-        }
+    //     if(item.quantity) {
+    //       const x= {
+    //         id: i+1,
+    //         name: item.variantName,
+    //         updatedAt: 'Updated 5m ago',
+    //         items: item.quantity,
+    //         total: item.subTotal
+    //       };
+    //       topSolds.push(x);
+    //     }
 
-      });
-    }
+    //   });
+    // }
 
     return topSolds;
   }
@@ -130,11 +131,11 @@ export class DashboardComponent implements OnInit{
 
 
   totalRevenues() {
-    const sales: Order[] =this.sales();
+    const sales: Order[] = this.sales();
     return this.totalPipe.transform(sales, 'saleTotal');
   }
   getTotalRevenues() {
-    return this.radomNumberPipe.transform(this. totalRevenues());
+    return this.radomNumberPipe.transform(this.totalRevenues());
   }
 
   stocks() {
@@ -146,82 +147,82 @@ export class DashboardComponent implements OnInit{
   }
 
   getLowStocks() {
-    const lowStocks=[];
-    const stocks: Stock[]=this.query.queries<Stock>(Tables.stocks, ` branchId="${this.branch.id}" AND canTrackingStock=true ORDER BY currentStock ASC`);
-    stocks.forEach((stock,i)=> {
-      if(i < 6) {
-        if(stock.lowStock && stock.currentStock < stock.lowStock) {
-          const variant=this.model.find<Variant>(Tables.variants,stock.variantId);
-          const product=this.model.find<Product>(Tables.products,variant.productId);
-          lowStocks.push({
-            id: stock.id,
-            name: variant.name==='Regular'?product.name:variant.name,
-            updatedAt: stock.updatedAt,
-            currentStock: stock.currentStock
-          });
-        }
-      }
+    const lowStocks = [];
+    // const stocks: Stock[]=this.query.queries<Stock>(Tables.stocks, ` branchId="${this.branch.id}" AND canTrackingStock=true ORDER BY currentStock ASC`);
+    // stocks.forEach((stock,i)=> {
+    //   if(i < 6) {
+    //     if(stock.lowStock && stock.currentStock < stock.lowStock) {
+    //       const variant=this.model.find<Variant>(Tables.variants,stock.variantId);
+    //       const product=this.model.find<Product>(Tables.products,variant.productId);
+    //       lowStocks.push({
+    //         id: stock.id,
+    //         name: variant.name==='Regular'?product.name:variant.name,
+    //         updatedAt: stock.updatedAt,
+    //         currentStock: stock.currentStock
+    //       });
+    //     }
+    //   }
 
-    });
-    return lowStocks;
+    // });
+    // return lowStocks;
   }
 
   getSaleStocks() {
     let stocks = [{ netProfit: 0, grossProfit: 0 }];
-    if (this.loadSales().length > 0) {
-        stocks=[];
-        this.loadSales().forEach(orderDetails => {
-          if (orderDetails && orderDetails.stockId) {
+    // if (this.loadSales().length > 0) {
+    //     stocks=[];
+    //     this.loadSales().forEach(orderDetails => {
+    //       if (orderDetails && orderDetails.stockId) {
 
-            stocks.push(this.loadOrderDetails(orderDetails));
-          }
-        });
-      }
+    // stocks.push(this.loadOrderDetails(orderDetails));
+    //     }
+    //   });
+    // }
     return stocks;
   }
 
 
   loadSales() {
-    const orderItems: OrderDetails[]=[];
-    const details: OrderDetails[]= this.model.loadAll<OrderDetails>(Tables.orderDetails);
-    this.sales().forEach(sale => {
-      if (sale) {
-        details.forEach(d=> {
-          if((!orderItems.find(dd=>dd.id===d.id)) && d.orderId===sale.id) {
-            orderItems.push(d);
-          }
-        });
-      }
-    });
+    const orderItems: OrderDetails[] = [];
+    // const details: OrderDetails[]= this.model.loadAll<OrderDetails>(Tables.orderDetails);
+    // this.sales().forEach(sale => {
+    //   if (sale) {
+    //     details.forEach(d=> {
+    //       if((!orderItems.find(dd=>dd.id===d.id)) && d.orderId===sale.id) {
+    //         orderItems.push(d);
+    //       }
+    //     });
+    //   }
+    // });
     return orderItems;
 
   }
 
   loadOrderDetails(orderDetails: OrderDetails) {
-    const stock: Stock = this.model.find<Stock>(Tables.stocks, orderDetails.stockId);
-    if(stock){
-      return { netProfit: orderDetails.subTotal-(orderDetails.quantity * stock.supplyPrice),
-        grossProfit: (orderDetails.taxAmount+orderDetails.subTotal)-(orderDetails.quantity * stock.supplyPrice) };
-    }else{
-      return  { netProfit: orderDetails.subTotal-(orderDetails.quantity * 0),
-        grossProfit: (orderDetails.taxAmount+orderDetails.subTotal)-(orderDetails.quantity * 0) };
-    }
-   
-   
+    // const stock: Stock = this.model.find<Stock>(Tables.stocks, orderDetails.stockId);
+    // if(stock){
+    //   return { netProfit: orderDetails.subTotal-(orderDetails.quantity * stock.supplyPrice),
+    //     grossProfit: (orderDetails.taxAmount+orderDetails.subTotal)-(orderDetails.quantity * stock.supplyPrice) };
+    // }else{
+    //   return  { netProfit: orderDetails.subTotal-(orderDetails.quantity * 0),
+    //     grossProfit: (orderDetails.taxAmount+orderDetails.subTotal)-(orderDetails.quantity * 0) };
+    // }
+    return
+
   }
 
-   topSoldsItem(): OrderDetails[] {
+  topSoldsItem() {
 
-    const ids=[];
-    const orderIds: Order[]=this.model.raw(`SELECT id
-    FROM orders WHERE branchId="${this.branch.id}"
-    ORDER BY updatedAt DESC LIMIT 5`) as Order[];
+    // const orderIds: Order[]=this.model.raw(`SELECT id
+    // FROM orders WHERE branchId="${this.branch.id}"
+    // ORDER BY updatedAt DESC LIMIT 5`) as Order[];
 
-    orderIds.forEach(d=> {
-      ids.push(`'${d.id}'`);
-    });
+    // orderIds.forEach(d=> {
+    //   ids.push(`'${d.id}'`);
+    // });
 
-    return this.model.raw(`SELECT *
-     FROM orderDetails WHERE  orderDetails.quantity > 0 AND orderId IN (${ids.join()}) ORDER BY updatedAt DESC LIMIT 5`);
+    // return this.model.raw(`SELECT *
+    //  FROM orderDetails WHERE  orderDetails.quantity > 0 AND orderId IN (${ids.join()}) ORDER BY updatedAt DESC LIMIT 5`);
+
   }
 }

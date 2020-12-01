6b47122e6fae0ec986689a8df8a9c9cf2398ba0e
Richard Muragijimana <beastar457@gmail.com>
2020-12-01 07:30:06 +0200

    remove failing tests

--- test/views/product/add/add_product_viewmodel_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  group('add product viewmodel ...', ()  async {
-    test('given name should updated the tmp product to given name and with the price for regular variant',(){
-      expect(1,1);
-    });
-  });
-}

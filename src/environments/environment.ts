352ca3e9c796510a53e043609f809792a874d1bf
Richard Muragijimana <beastar457@gmail.com>
2020-12-15 11:28:36 +0200

    pause on testing

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const AppConfig = {
   environment: 'LOCAL',
 }
 export const environment = {
-  production: false,
+  production: false, //TODO: this should be fals in testing for us to bypass the payment remember to make it true otherwise.
   paymentUrl: 'https://mysterious-depths-19225.herokuapp.com/',
   appUrl: 'http://localhost:8000/', // domains when developing should listen on port 8001 so it can be routed propper.
 

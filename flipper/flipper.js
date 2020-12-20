4af5b891737dcfcd19a2ebd8a612e5886de8850a
Richard Muragijimana <beastar457@gmail.com>
2020-12-20 20:44:12 +0200

    standarize the path

--- flipper/flipper.js
+++ flipper/flipper.js
@@ -2,7 +2,8 @@ var exec = require('child_process').exec,
   child
 // the bellow line only work on windows
 // NOTE:Our support team will help user to setup this.
-child = exec('java -jar C:\\Users\\Sone\\Dropbox\\sync\\flipper-1.0.0-SNAPSHOT.jar', function (error, stdout, stderr) {
+let path = process.env.Flipper
+child = exec('java -jar ' + path, function (error, stdout, stderr) {
   console.log('stdout: ' + stdout)
   console.log('stderr: ' + stderr)
   if (error !== null) {

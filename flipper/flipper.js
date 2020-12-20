99137bb7fa131ff66dc3f4064239923c5a1d1ca7
Richard Muragijimana <beastar457@gmail.com>
2020-12-20 20:25:47 +0200

    builtin server...

--- flipper/flipper.js
+++ flipper/flipper.js
@@ -2,7 +2,7 @@ var exec = require('child_process').exec,
   child
 // the bellow line only work on windows
 // NOTE:Our support team will help user to setup this.
-child = exec('java -jar D:\\demos\\desktop\\flipper\\flipper-0.0.1-SNAPSHOT.jar', function (error, stdout, stderr) {
+child = exec('java -jar C:\\Users\\Sone\\Dropbox\\sync\\flipper-1.0.0-SNAPSHOT.jar', function (error, stdout, stderr) {
   console.log('stdout: ' + stdout)
   console.log('stderr: ' + stderr)
   if (error !== null) {

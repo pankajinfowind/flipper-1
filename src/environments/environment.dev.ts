a4a0b40bc66874e47081f6cbb5758812b69ab0d8
Richard Muragijimana <beastar457@gmail.com>
2021-01-11 20:36:27 +0200

    about to go back

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,9 +1,25 @@
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `index.ts`, but if you do
-// `ng build --env=prod` then `index.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
-
 export const AppConfig = {
   production: false,
-  environment: 'DEV',
+  environment: 'LOCAL',
+}
+export const environment = {
+  production: false, //TODO: this should be fals in testing for us to bypass the payment remember to make it true otherwise.
+  paymentUrl: 'https://mysterious-depths-19225.herokuapp.com/',
+  appUrl: 'http://localhost:8000/', // domains when developing should listen on port 8001 so it can be routed propper.
+
+  pusher: {
+    key: 'a5c8bceaeeac6065d8de',
+    cluster: 'ap2',
+    forceTLS: true,
+    encrypted: true,
+  },
+  config: {
+    // https://www.techiediaries.com/angular-firebase/angular-9-8-firestore-database-crud-tutorial/
+    apiKey: 'AIzaSyCpnbw9i23T0237jgd2ladtPgsGYbmioqA',
+    authDomain: 'yegobox-2ee43.firebaseapp.com',
+    databaseURL: 'https://yegobox-2ee43.firebaseio.com',
+    projectId: 'yegobox-2ee43',
+    storageBucket: 'yegobox-2ee43.appspot.com',
+  },
+  settings: { timestampsInSnapshots: true },
 }

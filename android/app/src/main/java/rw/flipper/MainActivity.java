842426415816f81ecad23f0793d3265070850a44
Richard Muragijimana <beastar457@gmail.com>
2021-01-14 21:48:17 +0200

    revert back to kotlin

--- android/app/src/main/java/rw/flipper/MainActivity.java
+++
@@ -1,7 +0,0 @@
-package rw.flipper;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-
-}
---
+++ android/app/src/main/kotlin/rw/flipper/MainActivity.kt
@@ -0,0 +1,6 @@
+package rw.flipper
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}

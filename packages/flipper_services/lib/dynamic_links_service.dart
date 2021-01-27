4d1ee2a447f80d162cd4a27cd8ea7a701a29a113
Richard Muragijimana <beastar457@gmail.com>
2021-01-27 19:56:55 +0200

    share the link to flipper app

--- packages/flipper_services/lib/dynamic_links_service.dart
+++ packages/flipper_services/lib/dynamic_links_service.dart
@@ -1,12 +1,15 @@
 library flipper_services;
 
 import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
+import 'locator.dart';
+import 'shared_state_service.dart';
 
 class DynamicLinkService {
   Future handleDynamicLink() async {
     // if the is opened with the link
     final PendingDynamicLinkData data =
         await FirebaseDynamicLinks.instance.getInitialLink();
+
     _handleDnamicLink(data);
     //app is opened from foreground
     FirebaseDynamicLinks.instance.onLink(
@@ -24,9 +27,10 @@ class DynamicLinkService {
   }
 
   Future<ShortDynamicLink> createDynamicLink() async {
+    final state = locator<SharedStateService>();
     final DynamicLinkParameters parameters = DynamicLinkParameters(
       uriPrefix: 'https://flipper.rw',
-      link: Uri.parse('https://flipper.rw/open?DA'),
+      link: Uri.parse('https://flipper.rw/open?' + state.user.id),
       androidParameters: AndroidParameters(
         packageName: 'rw.flipper',
         minimumVersion: 1,

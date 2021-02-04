01e06ffd17d733c97fe0e141e5271998c6299468
Richard Muragijimana <beastar457@gmail.com>
2021-02-04 18:30:49 +0200

    re-work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VendorsModule } from '@enexus/flipper-vendors';
 
 @NgModule({
   declarations: [
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    VendorsModule,
     AppRoutingModule
   ],
   providers: [],

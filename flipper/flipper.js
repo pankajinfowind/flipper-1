74ceff03effaf5c79b09df5e34e2cc2dd10e5dda
Richard Muragijimana <beastar457@gmail.com>
2021-02-06 10:51:00 +0200

    jar

--- flipper/flipper.js
+++ flipper/flipper.js
@@ -6,7 +6,7 @@ const { autoUpdater } = require('electron-updater')
 
 autoUpdater.logger = log
 
-var server = path.join(__dirname + '/../server/', 'flipper-1.20.3-client')
+var server = path.join(__dirname + '/../server/', 'flipper-1.20.3-client.jar')
 
 var dd = server.replace(/\app.asar/g, '')
 var final = dd.replace(/\\\\/g, '\\')

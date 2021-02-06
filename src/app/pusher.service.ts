bcd0b06b258a167762fcb00647e822cb44b9b848
Richard Muragijimana <beastar457@gmail.com>
2021-02-06 10:35:42 +0200

    use extra resources instead of extraFiles

--- src/app/pusher.service.ts
+++ src/app/pusher.service.ts
@@ -17,7 +17,7 @@ export class PusherService {
       encrypted: environment.pusher.cluster,
     })
 
-    const userId = this.user.get('userIdNew') as string
+    const userId = this.user.get('userIdNew')
 
     this.handleDomainMessage = this.pusher.subscribe('handle-domain-message-flipper.' + userId)
     this.paymentApproved = this.pusher.subscribe('payment-message-flipper.' + userId)
